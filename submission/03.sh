23fa3b35dd95d0261ca54e94bd0947b05f0ee847
Abubakar Sadiq <53898703+sadeeqrabiu@users.noreply.github.com>
2026-04-19 03:07:36 +0100

    Update: capstone connfigured

--- submission/03.sh
+++ submission/03.sh
@@ -1,1 +1,2 @@
 # Which tx in block 216,351 spends the coinbase output of block 216,128?
+cb=$(bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 216128) 1 | jq -r '.tx[0]'); bitcoin-cli -signet getblock $(bitcoin-cli -signet getblockhash 216351) 2 | jq -r --arg cb "$cb" '.tx[] | select(any(.vin[]; .txid == $cb)) | .txid'
